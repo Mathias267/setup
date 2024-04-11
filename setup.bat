53d2caea1266dbc35b1fe56d5a07ee643d145e85
Mathias <mathiasmoreiraisrael@gmail.com>
2024-04-11 11:39:36 -0300

    setup feito testado

---
+++ setup.bat
@@ -0,0 +1,10 @@
+git config --global user.name Mathias
+git config --global user.email mathiasmoreiraisrael@gmail.com
+
+cd C:\Users\MATHIASISRAELMOREIRA\
+
+git clone https://github.com/Mathias267/Caesar-Cipher.git
+cd Caesar-Cipher 
+
+code .
+ 
